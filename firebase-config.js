b3b347317faf19036f87d104bff3268c359afd8d
lordlaudemir-eng <lordlaudemir@gmail.com>
2026-06-14 17:24:03 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBcYDev71DrTrqszev3oL23mPRhezZV6DI",
+  authDomain: "senhas-a376f.firebaseapp.com",
+  projectId: "senhas-a376f",
+  storageBucket: "senhas-a376f.firebasestorage.app",
+  messagingSenderId: "100373736889",
+  appId: "1:100373736889:web:67f750a4343d59f9f45a68"
+};
